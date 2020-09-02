03224ef76e4589907b59cfcb088328c294c063e2
aortigosa <alexandre.ortigosa@gmail.com>
2020-09-02 14:10:20 +0200

    add use case to factory

--- domain/cacas/UseCases/factory.js
+++ domain/cacas/UseCases/factory.js
@@ -1,1 +1,10 @@
-export default class CacaUseCasesFactory {}
+import AddCacaUseCase from "./AddCacaUseCase";
+import CacaRepositoryFactory from "../Repositories/factory";
+import CacaRequestsFactory from "../Requests/factory";
+
+export default class CacaUseCasesFactory {
+    static addCacaUseCase = ({config}) => new AddCacaUseCase({
+        cacaRepository: CacaRepositoryFactory.localMemoryCacaRepository({config}),
+        requestFactory: CacaRequestsFactory.addCacaRequest
+    })
+}
