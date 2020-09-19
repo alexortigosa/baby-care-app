4596931248c406ce1a2b91a1431baeaf614ae68f
aortigosa <alexandre.ortigosa@gmail.com>
2020-09-20 00:29:22 +0200

    tomas screen

--- screens/TomasScreen.js
+++ screens/TomasScreen.js
@@ -1,71 +1,77 @@
 import * as React from 'react'
-import {StyleSheet, Text, View} from 'react-native'
-import {Ionicons} from '@expo/vector-icons'
-import * as WebBrowser from 'expo-web-browser'
-import {RectButton, ScrollView} from 'react-native-gesture-handler'
-import {Button} from 'react-native-elements'
-import Icon from 'react-native-vector-icons/FontAwesome'
+import {StyleSheet, Text, View, FlatList, StatusBar} from 'react-native'
+import {format} from 'date-fns'
+
+import DateTimePickerComponent from '../components/DateTimePickerComponent'
+
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome'
+import domain from '../instance'
+
+const Item = ({title, isRigth}) => (
+  <View style={styles.item}>
+    <FontAwesomeIcon icon="syringe" color="yellow" size={32} />
+    <Text style={styles.title}>{title}</Text>
+    <Text> Toma {isRigth ? 'Derecha' : 'Izquierda'}</Text>
+  </View>
+)
 
 export default function TomasScreen() {
+  const [tomasList, setTomasList] = React.useState([])
+  const [date, setDate] = React.useState(Date.now())
+  React.useEffect(() => {
+    console.log('use effect get cacas')
+    async function getCacas() {
+      console.log('get tomas')
+      const {list} = await domain.get('get_toma_list_by_date').execute({date})
+      console.log({message: 'tomas seted', list})
+      setTomasList(list)
+    }
+    getCacas()
+  }, [date])
+
+  const renderItem = ({item}) => {
+    return <Item title={format(item.date, 'dd-MM-yyyy HH:mm')} isRigth />
+  }
+
+  const setCustomDate = date => {
+    console.log({message: 'setCustomDate', date})
+    setDate(date)
+  }
+
   return (
-    <ScrollView
-      style={styles.container}
-      contentContainerStyle={styles.contentContainer}
-    >
-      <OptionButton
-        icon="md-school"
-        label="Read the Expo documentation"
-        onPress={() => WebBrowser.openBrowserAsync('https://docs.expo.io')}
-      />
-
-      <OptionButton
-        icon="md-compass"
-        label="Read the React Navigation documentation"
-        onPress={() =>
-          WebBrowser.openBrowserAsync('https://reactnavigation.org')
-        }
-      />
-
-      <OptionButton
-        icon="ios-chatboxes"
-        label="Ask a question on the forums"
-        onPress={() => WebBrowser.openBrowserAsync('https://forums.expo.io')}
-        isLastOption
-      />
-    </ScrollView>
+    <View style={styles.container}>
+      <View>
+        <DateTimePickerComponent sendFunction={setCustomDate} />
+      </View>
+      <View>
+        <FlatList
+          data={tomasList}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+        />
+      </View>
+    </View>
   )
-}
-
-function OptionButton({icon, label, onPress, isLastOption}) {
-  return <Button onPress={onPress} title={label} />
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fafafa'
+    marginTop: StatusBar.currentHeight || 0
   },
-  contentContainer: {
-    paddingTop: 15,
-    justifyContent: 'space-between'
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    fontSize: 32
   },
-  optionIconContainer: {
-    marginRight: 12
+  title: {
+    fontSize: 32
   },
-  option: {
-    backgroundColor: '#fdfdfd',
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderBottomWidth: 0,
-    borderColor: '#ededed'
-  },
-  lastOption: {
-    borderBottomWidth: StyleSheet.hairlineWidth
-  },
-  optionText: {
-    fontSize: 15,
-    alignSelf: 'flex-start',
-    marginTop: 1
+  icon: {
+    fontSize: 32
   }
 })
