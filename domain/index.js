27cab9b9a386e03fe415ce359e4480d9815df3fa
aortigosa <alexandre.ortigosa@gmail.com>
2020-09-20 00:07:34 +0200

    exposing new use cases

--- domain/index.js
+++ domain/index.js
@@ -2,6 +2,7 @@ import {EntryPointFactory} from '@s-ui/domain'
 import Config from './config'
 import UsersUseCasesFactory from './users/UseCases/factory'
 import CacaUseCasesFactory from './cacas/UseCases/factory'
+import TomaUseCasesFactory from './tomas/UseCases/factory'
 
 const config = new Config()
 
@@ -14,7 +15,9 @@ const useCases = {
   login_users_use_case: UsersUseCasesFactory.loginUsersUseCase({config}),
   logout_users_use_case: UsersUseCasesFactory.logoutUsersUseCase({config}),
   add_caca_use_case: CacaUseCasesFactory.addCacaUseCase({config}),
-  get_caca_list_by_date: CacaUseCasesFactory.getCacaListByDateUseCase({config})
+  get_caca_list_by_date: CacaUseCasesFactory.getCacaListByDateUseCase({config}),
+  add_toma_use_case: TomaUseCasesFactory.addTomaUseCase({config}),
+  get_toma_list_by_date: TomaUseCasesFactory.getTomaListByDateUseCase({config})
 }
 // Export entry point
 export default EntryPointFactory({config, useCases})
